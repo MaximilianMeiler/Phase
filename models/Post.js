34f0e4352216c6a7af554a77a2a65d4732e20a82
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-05-18 15:48:42 -0400

    rip models

--- models/Post.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema, model } = mongoose;
-
-const postSchema = new Schema({
-  imageLink: String,
-});
-
-const Post = model('Post', postSchema);
-export default Post;
